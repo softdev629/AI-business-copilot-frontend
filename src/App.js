5ce4f59d834857be433b624e7b9d7d2cf569c602
Leo Kott <121113067+violetstar701@users.noreply.github.com>
2023-02-28 04:28:45 -0500

    separate upload & chat

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 // import libraries
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import React from "react";
 import "./App.css";
 import "antd/dist/reset.css";
 
-import PDFChatQA from "./components/chatqa";
+import PDFUpload from "./components/chatqa/pdf-upload-dom";
+import ChatBox from "./components/chatqa/chatbox";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<PDFChatQA />} />
+          <Route path="/upload" element={<PDFUpload />} />
+          <Route path="/chat" element={<ChatBox />} />
         </Routes>
       </Router>
     </div>
